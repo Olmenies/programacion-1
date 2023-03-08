fbb1c20bc099a34fbc5ba804bf10e088cf40e9a5
Nicolás Esteban Olmedo <olmedonies@gmail.com>
2023-03-08 08:58:48 -0300

    tp_03_ejercicio_04 - Done

---
+++ programacion_1_tp_03/tp_03_ejercicio_05/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace tp_03_ejercicio_05
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            GroupBoxInput = new GroupBox();
+            label2 = new Label();
+            label1 = new Label();
+            TextBoxMax = new TextBox();
+            TextBoxMin = new TextBox();
+            ListBox = new ListBox();
+            ButtonCalculate = new Button();
+            GroupBoxInput.SuspendLayout();
+            SuspendLayout();
+            // 
+            // GroupBoxInput
+            // 
+            GroupBoxInput.Controls.Add(label2);
+            GroupBoxInput.Controls.Add(label1);
+            GroupBoxInput.Controls.Add(TextBoxMax);
+            GroupBoxInput.Controls.Add(TextBoxMin);
+            GroupBoxInput.Location = new Point(12, 12);
+            GroupBoxInput.Name = "GroupBoxInput";
+            GroupBoxInput.Size = new Size(238, 100);
+            GroupBoxInput.TabIndex = 0;
+            GroupBoxInput.TabStop = false;
+            GroupBoxInput.Text = "Input";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(117, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Max";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(28, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Min";
+            // 
+            // TextBoxMax
+            // 
+            TextBoxMax.Location = new Point(117, 37);
+            TextBoxMax.Name = "TextBoxMax";
+            TextBoxMax.Size = new Size(100, 23);
+            TextBoxMax.TabIndex = 1;
+            // 
+            // TextBoxMin
+            // 
+            TextBoxMin.Location = new Point(11, 37);
+            TextBoxMin.Name = "TextBoxMin";
+            TextBoxMin.Size = new Size(100, 23);
+            TextBoxMin.TabIndex = 0;
+            // 
+            // ListBox
+            // 
+            ListBox.FormattingEnabled = true;
+            ListBox.ItemHeight = 15;
+            ListBox.Location = new Point(256, 18);
+            ListBox.Name = "ListBox";
+            ListBox.Size = new Size(120, 94);
+            ListBox.TabIndex = 1;
+            // 
+            // ButtonCalculate
+            // 
+            ButtonCalculate.Location = new Point(12, 118);
+            ButtonCalculate.Name = "ButtonCalculate";
+            ButtonCalculate.Size = new Size(75, 23);
+            ButtonCalculate.TabIndex = 2;
+            ButtonCalculate.Text = "Calculate";
+            ButtonCalculate.UseVisualStyleBackColor = true;
+            ButtonCalculate.Click += ButtonCalculate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(487, 166);
+            Controls.Add(ButtonCalculate);
+            Controls.Add(ListBox);
+            Controls.Add(GroupBoxInput);
+            Name = "Form1";
+            Text = "Trabajo Práctico 03 - Ejercicio 05";
+            GroupBoxInput.ResumeLayout(false);
+            GroupBoxInput.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox GroupBoxInput;
+        private Label label2;
+        private Label label1;
+        private TextBox TextBoxMax;
+        private TextBox TextBoxMin;
+        private ListBox ListBox;
+        private Button ButtonCalculate;
+    }
+}
---
+++ programacion_1_tp_03/tp_03_ejercicio_05/Form1.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics;
+
+namespace tp_03_ejercicio_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /********** Events **********/
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            ListBox.Items.Clear();
+            bool isInputValid = ValidateInput();
+            int[] primesArray = LookForPrimes(int.Parse(TextBoxMin.Text), int.Parse(TextBoxMax.Text)); // This should be divided on a method to generate an array of ints and a method to print the elements on the listbox
+            PrintToListBox(primesArray);
+        }
+
+        /********** Methods **********/
+        /// <summary>
+        /// Validates if the input values are parseable to int
+        /// </summary>
+        /// <returns>True if values are parseable to int or false if not</returns>
+        public bool ValidateInput()
+        {
+            foreach (Control control in GroupBoxInput.Controls)
+            {
+                if (control is TextBox)
+                {
+                    bool success = int.TryParse(control.Text, out _);
+                    if (!success) { return false; }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if a provided number is prime
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static bool CheckIfPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                //if ((i == 1) || (i == 0)) { continue; }
+                if ((num % i == 0)) { return false; }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Iterates between min and max values looking for primes numbers
+        /// </summary>
+        /// /// <param name="min">Minimum value</param>
+        /// /// <param name="max">Maximum value</param>
+        /// <returns>An array containing all prime numbers between the limits provided</returns>
+        public static int[] LookForPrimes(int min, int max)
+        {
+            int[] ints = new int[0];
+            for (int i = min; i < max; i++)
+            {
+                if ((i == 0) || (i == 1))
+                {
+                    continue;
+                }
+                else
+                {
+                    bool success = CheckIfPrime(i);
+                    //if (success) { ListBox.Items.Add(i.ToString()); }
+                    if (success)
+                    {
+                        Array.Resize(ref ints, ints.Length + 1);
+                        ints[ints.Length - 1] = i;
+                    }
+                }
+            }
+
+            return ints;
+        }
+
+        /// <summary>
+        /// Prints an array to a ListBox
+        /// </summary>
+        /// <param name="primesArray"></param>
+        public void PrintToListBox(int[] primesArray)
+        {
+            foreach (int i in primesArray)
+            {
+                ListBox.Items.Add(i.ToString());
+            }
+        }
+    }
+}
