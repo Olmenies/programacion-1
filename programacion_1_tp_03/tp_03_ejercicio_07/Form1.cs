4bd03078f34ca7883f6dc46ffee8aab354a28504
Nicolás Esteban Olmedo <olmedonies@gmail.com>
2023-03-08 15:42:36 -0300

    tp_03_ejercicio_07 - Done

--- programacion_1_tp_03/tp_03_ejercicio_07/Form1.cs
+++ programacion_1_tp_03/tp_03_ejercicio_07/Form1.cs
@@ -9,16 +9,7 @@ namespace tp_03_ejercicio_07
         public Form1()
         {
             InitializeComponent();
-            
-            dataTable = new DataTable();
-            dataTable.Columns.Add("Amount", typeof(float));
-            dataTable.Columns.Add("TNA", typeof(float));
-            dataTable.Columns.Add("Days", typeof(float));
-            dataTable.Columns.Add("Total", typeof(float));
-
-            DataGridViewResult.DataSource = dataTable;
-            DataGridViewResult.ReadOnly = true;
-            DataGridViewResult.ColumnHeadersVisible = true;
+            InitializeDataTable();
         }
 
         /*********** Events *********/
@@ -28,21 +19,23 @@ namespace tp_03_ejercicio_07
             bool isInputValid = ValidateInput();
             if (isInputValid)
             {
-                MessageBox.Show("Cumbia");
-                float result = CalculateTotal(float.Parse(TextBoxAmount.Text), float.Parse(TextBoxTNA.Text), float.Parse(TextBoxDays.Text));
-                DataRow row = dataTable.NewRow();
-                row["Amount"] = float.Parse(TextBoxAmount.Text);
-                row["TNA"] = float.Parse(TextBoxTNA.Text);
-                row["Days"] = float.Parse(TextBoxDays.Text);
-                row["Total"] = result;
-
-                dataTable.Rows.Add(row);   
+                float amount = float.Parse(TextBoxAmount.Text);
+                float tna = float.Parse(TextBoxTNA.Text);
+                float days = float.Parse(TextBoxDays.Text);
+                
+                float result = CalculateTotal(amount, tna, days);
+                SaveToDataTable(amount, tna, days, result);
             }
         }
 
         /********* Methods *********/
+        /// <summary>
+        /// Validates if the inputs are parseable to float
+        /// </summary>
+        /// <returns></returns>
         private bool ValidateInput()
         {
+            //@@@TODO: This coud be improved
             if (float.TryParse(TextBoxAmount.Text, out float amount))
             {
                 if (amount < 0)
@@ -80,10 +73,50 @@ namespace tp_03_ejercicio_07
             return true;
         }
 
+        /// <summary>
+        /// Calculates the interest using the formula amount * tna * days / 36500 
+        /// </summary>
+        /// <param name="amount">Amount of money to be used</param>
+        /// <param name="tna">TNA</param>
+        /// <param name="days">Amount of days</param>
+        /// <returns>Returns the interests</returns>
         private static float CalculateTotal(float amount, float tna, float days)
         {
             return amount * tna * days / 365000;
         }
 
+        /// <summary>
+        /// Initializes a new DataTable and sets some configuration to DataGridView
+        /// </summary>
+        private void InitializeDataTable()
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("Amount", typeof(float));
+            dataTable.Columns.Add("TNA", typeof(float));
+            dataTable.Columns.Add("Days", typeof(float));
+            dataTable.Columns.Add("Total", typeof(float));
+
+            DataGridViewResult.DataSource = dataTable;
+            DataGridViewResult.ReadOnly = true;
+            DataGridViewResult.ColumnHeadersVisible = true;
+        }
+
+        /// <summary>
+        /// Saves data to a DataTable
+        /// </summary>
+        /// <param name="amount">Amount of money to be used</param>
+        /// <param name="tna">TNA</param>
+        /// <param name="days">Amount of days</param>
+        /// <param name="total">Interests</param>
+        private void SaveToDataTable(float amount, float tna, float days, float total)
+        {
+            DataRow row = dataTable.NewRow();
+            row["Amount"] = float.Parse(TextBoxAmount.Text);
+            row["TNA"] = float.Parse(TextBoxTNA.Text);
+            row["Days"] = float.Parse(TextBoxDays.Text);
+            row["Total"] = total;
+
+            dataTable.Rows.Add(row);
+        }
     }
 }
