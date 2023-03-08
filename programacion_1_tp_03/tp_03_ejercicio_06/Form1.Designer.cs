4b7bcb5aeacb39bf2f8de6f88b3ca5eb20513813
Nicolás Esteban Olmedo <olmedonies@gmail.com>
2023-03-08 10:50:21 -0300

    tp_03_ejercicio_06 - Done

---
+++ programacion_1_tp_03/tp_03_ejercicio_06/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace tp_03_ejercicio_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            TextBoxInput = new TextBox();
+            ButtonCalculate = new Button();
+            ListBox = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Max";
+            // 
+            // TextBoxInput
+            // 
+            TextBoxInput.Location = new Point(12, 27);
+            TextBoxInput.Name = "TextBoxInput";
+            TextBoxInput.Size = new Size(100, 23);
+            TextBoxInput.TabIndex = 2;
+            // 
+            // ButtonCalculate
+            // 
+            ButtonCalculate.Location = new Point(12, 56);
+            ButtonCalculate.Name = "ButtonCalculate";
+            ButtonCalculate.Size = new Size(100, 50);
+            ButtonCalculate.TabIndex = 3;
+            ButtonCalculate.Text = "Calcular";
+            ButtonCalculate.UseVisualStyleBackColor = true;
+            ButtonCalculate.Click += ButtonCalculate_Click;
+            // 
+            // ListBox
+            // 
+            ListBox.FormattingEnabled = true;
+            ListBox.ItemHeight = 15;
+            ListBox.Location = new Point(118, 12);
+            ListBox.Name = "ListBox";
+            ListBox.Size = new Size(120, 94);
+            ListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(364, 134);
+            Controls.Add(ListBox);
+            Controls.Add(ButtonCalculate);
+            Controls.Add(TextBoxInput);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Trabajo Práctico 03 - Ejercicio 06";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox TextBoxInput;
+        private Button ButtonCalculate;
+        private ListBox ListBox;
+    }
+}
---
+++ programacion_1_tp_03/tp_03_ejercicio_06/Form1.cs
@@ -0,0 +1,71 @@
+namespace tp_03_ejercicio_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /********** Events *********/
+        private void ButtonCalculate_Click(object sender, EventArgs e)
+        {
+            bool isInputValid = ValidateInput(TextBoxInput.Text);
+            if (isInputValid)
+            {
+                uint[] fibonacciArray = GenerateFibonacci(uint.Parse(TextBoxInput.Text));
+                PrintToListBox(fibonacciArray);
+            }
+        }
+
+        /********* Methods *********/
+
+        /// <summary>
+        /// Validates if the input is parseable to uint
+        /// </summary>
+        /// <param name="input">Value to be evaluated</param>
+        /// <returns></returns>
+        private static bool ValidateInput(string input)
+        {
+            if (!uint.TryParse(input, out _))
+            {
+                MessageBox.Show("Input is not valid", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Generates the Fibonacci sequence
+        /// </summary>
+        /// <param name="maxNumber">Amount of elments of the sequence desired</param>
+        /// <returns>An uint array with the Fibonacci sequence</returns>
+        private static uint[] GenerateFibonacci(uint maxNumber)
+        {
+            uint[] tempArray = { 0, 1 };
+
+            for (int i = 0; i < maxNumber; i++)
+            {
+                Array.Resize(ref tempArray, tempArray.Length + 1);
+                tempArray[tempArray.Length - 1] = tempArray[tempArray.Length - 2] + tempArray[tempArray.Length - 3];
+            }
+
+            return tempArray;
+        }
+
+        /// <summary>
+        /// Prints an array into a ListBox
+        /// </summary>
+        /// <param name="fibonacciArray">Array to be printed</param>
+        private void PrintToListBox(uint[] fibonacciArray)
+        {
+            foreach (uint item in fibonacciArray)
+            {
+                ListBox.Items.Add(item.ToString());
+            }
+        }
+    }
+}
